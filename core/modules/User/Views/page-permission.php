877a4cdcc07d54e0679b87745c6915040c622151
noelma <noel-mathieu@hotmail.fr>
2019-04-27 17:10:55 +0200

    #55 Peaufinage du champ de recherche des permissions.

--- core/modules/User/Views/page-permission.php
+++ core/modules/User/Views/page-permission.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-sm-12">
         <div class="form-group">
-            <input type="text" id="search" class="form-control" placeholder="Rechercher permission (exemple: voir, éditer, supprimer...)" onkeyup="searchPermission();">
+            <input type="text" id="search" class="form-control" placeholder="Rechercher des permissions (exemple: voir, éditer, supprimer...)" onkeyup="searchPermission();" autofocus>
         </div>
         <form method="post" action="<?php echo $link_update ?>">
             <fieldset class="responsive">
@@ -12,31 +12,32 @@
                             <th>Droit</th>
                             <?php foreach ($roles as $role): ?>
 
-                                <th><?php echo $role[ 'role_label' ]; ?></th>
+                            <th><?php echo $role[ 'role_label' ]; ?></th>
                             <?php endforeach; ?>
 
                         </tr>
                     </thead>
                     <tbody id="table-permission">
-                        <?php foreach ($modules as $key => $module): ?>
+                    <?php foreach ($modules as $key => $module): ?>
 
-                            <tr><td colspan="<?php echo $colspan; ?>" class="permission-module"><?php echo $key; ?></td></tr>
-                            <?php foreach ($module as $key => $permission): ?>
+                        <tr><td id="<?php echo $key; ?>" colspan="<?php echo $colspan; ?>" class="permission-module"><?php echo $key; ?></td></tr>
+                        <?php foreach ($module as $key => $permission): ?>
 
-                            <tr id="<?php echo $key ?>">
-                                <th><?php echo $permission[ 'action' ] ?></th>
-                                <?php foreach ($permission[ 'roles' ] as $role => $checked): ?>
-                                    <?php $name = $role . '[' . $key . ']' ?>
+                        <tr id="<?php echo $key ?>">
+                            <th><?php echo $permission[ 'action' ] ?></th>
+                            <?php foreach ($permission[ 'roles' ] as $role => $checked): ?>
+                            <?php $name = $role . '[' . $key . ']' ?>
 
-                                <td data-title="<?php echo $roles[$role - 1]['role_label']; ?>">
-                                    <input type="checkbox" name="<?php echo $name ?>" id="<?php echo $name ?>" value="<?php echo $key ?>" <?php echo $checked ?>>
-                                    <label for="<?php echo $name ?>"><i class="ui"></i></label>
-                                </td>
-                                <?php endforeach; ?>
-
-                            </tr>
+                            <td data-title="<?php echo $roles[ $role - 1 ][ 'role_label' ]; ?>">
+                                <input type="checkbox" name="<?php echo $name ?>" id="<?php echo $name ?>" value="<?php echo $key ?>" <?php echo $checked ?>>
+                                <label for="<?php echo $name ?>"><i class="ui"></i></label>
+                            </td>
                             <?php endforeach; ?>
+
+                        </tr>
                         <?php endforeach; ?>
+                    <?php endforeach; ?>
+
                     </tbody>
                 </table>
             </fieldset>
@@ -49,14 +50,22 @@
     function searchPermission()
     {
         var input = document.getElementById('search').value;
-        var reg = new RegExp(input, 'i');
+        var reg   = new RegExp(input, 'i');
         Object.keys(modules).forEach(function (module)
         {
+            var module_hide = true;
             Object.keys(modules[module]).forEach(function (permission) {
-                document.getElementById(permission).style.display = !reg.test(modules[module][permission].action)
-                        ? "none"
-                        : "";
+                if (!reg.test(modules[module][permission].action)) {
+                    document.getElementById(permission).style.display = "none";
+                } else {
+                    document.getElementById(permission).style.display = "";
+                    module_hide = false;
+                }
             });
+
+            document.getElementById(module).style.display = module_hide
+                    ? "none"
+                    : "";
         });
     }
 </script>
