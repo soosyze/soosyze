e379d069152d4eed3a0a6cf5dd126da5b922f74b
noelma <noel-mathieu@hotmail.fr>
2019-06-06 10:03:04 +0200

    Ajout d'un style sticky aux thèmes d'administration.

--- core/modules/Config/Views/page-config.php
+++ core/modules/Config/Views/page-config.php
@@ -1,7 +1,7 @@
 
 <div class="row">
     <?php if (!empty($form)): ?>
-    <div class="col-md-3">
+    <div class="col-md-3 sticky">
         <?php echo $block[ 'menu_config' ]; ?>
     </div>
     <div class="col-md-9">
