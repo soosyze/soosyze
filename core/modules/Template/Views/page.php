31ff169eb2709ee829cbd704a26d28241484f931
noelma <noel-mathieu@hotmail.fr>
2019-08-02 16:43:53 +0200

    #66 Régression. Simplification du service Temlplate.

--- core/modules/Template/Views/page.php
+++ core/modules/Template/Views/page.php
@@ -5,20 +5,20 @@
     <?php endif; ?>
 
     <h1><?php echo $title_main; ?></h1>
-    <?php echo $block[ 'main_menu' ]; ?>
+    <?php echo $section[ 'main_menu' ]; ?>
 
 </header>
 <div class="main-wrapper">
     <div class="main-content">
         <div class="container">
-            <?php if (!empty($block[ 'messages' ])): ?>
-                <?php echo $block[ 'messages' ]; ?>
+            <?php if (!empty($section[ 'messages' ])): ?>
+                <?php echo $section[ 'messages' ]; ?>
             <?php endif; ?>
-            <?php echo $block[ 'content' ]; ?>
+            <?php echo $section[ 'content' ]; ?>
 
         </div>
     </div> <!-- /.main-content -->
 </div>
 <footer>
-    <?php echo $block[ 'second_menu' ]; ?>
+    <?php echo $section[ 'second_menu' ]; ?>
 </footer>
