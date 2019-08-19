1bd83999261cdc914f6f862629b76ad413942da2
noelma <noel-mathieu@hotmail.fr>
2019-08-19 14:39:41 +0200

    #80 Ajout de la gestion des fenêtres modales aux thèmes.

--- core/themes/Bootstrap3/assets/js/script.js
+++ core/themes/Bootstrap3/assets/js/script.js
@@ -9,4 +9,18 @@ $(function () {
             $('#btn_up').fadeIn();
         }
     });
+    /* MODAL */
+    var target = '';
+    $('[data-toogle="modal"]').click(function (evt) {
+        evt.preventDefault();
+        target = $(this).data('target');
+        $(target).show();
+        $('body').toggleClass('modal-open');
+    });
+    $(window).click(function (evt) {
+        if (evt.target.className === 'modal' || evt.target.className === 'close') {
+            $(target).hide();
+            $('body').toggleClass('modal-open');
+        }
+    });
 });
--- core/themes/QuietBlue/assets/js/script.js
+++ core/themes/QuietBlue/assets/js/script.js
@@ -19,4 +19,18 @@ $(function () {
     $('#toogle_menu').click(function () {
         $('#menu').slideToggle();
     });
+    /* MODAL */
+    var target = '';
+    $('[data-toogle="modal"]').click(function (evt) {
+        evt.preventDefault();
+        target = $(this).data('target');
+        $(target).show();
+        $('body').toggleClass('modal-open');
+    });
+    $(window).click(function (evt) {
+        if (evt.target.className === 'modal' || evt.target.className === 'close') {
+            $(target).hide();
+            $('body').toggleClass('modal-open');
+        }
+    });
 });
