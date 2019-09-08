276764858bd1e6607d56f1bbd7d5c6b81c64a8ef
noelma <noel-mathieu@hotmail.fr>
2019-09-08 23:09:00 +0200

    Simplification et ajouts d'icônes au modèle social du module Block.

--- core/modules/Block/Views/blocks/block-social.php
+++ core/modules/Block/Views/blocks/block-social.php
@@ -1,9 +1,19 @@
 
 <ul class="icons">
-    <li><a href="#" class="icon fab fa-twitter alt"><span class="label">Twitter</span></a></li>
-    <li><a href="#" class="icon fab fa-facebook alt"><span class="label">Facebook</span></a></li>
-    <li><a href="#" class="icon fab fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
-    <li><a href="#" class="icon fab fa-instagram alt"><span class="label">Instagram</span></a></li>
-    <li><a href="#" class="icon fab fa-github alt"><span class="label">GitHub</span></a></li>
-    <li><a href="#" class="icon fab fa-dribbble alt"><span class="label">Dribbble</span></a></li>
+    <li><a href="#" class="fab fa-blogger"><i>Blogger</i></a></li>
+    <li><a href="#" class="fab fa-dribbble"><i>Dribbble</i></a></li>
+    <li><a href="#" class="fab fa-facebook"><i>Facebook</i></a></li>
+    <li><a href="#" class="fab fa-github"><i>GitHub</i></a></li>
+    <li><a href="#" class="fab fa-instagram"><i>Instagram</i></a></li>
+    <li><a href="#" class="fab fa-linkedin"><i>Linkedin</i></a></li>
+    <li><a href="#" class="fab fa-mastodon"><i>Mastodon</i></a></li>
+    <li><a href="#" class="fab fa-slack-hash"><i>Slack</i></a></li>
+    <li><a href="#" class="fab fa-snapchat"><i>Snapchat</i></a></li>
+    <li><a href="#" class="fab fa-soundcloud"><i>Soundcloud</i></a></li>
+    <li><a href="#" class="fab fa-spotify"><i>Spotify</i></a></li>
+    <li><a href="#" class="fab fa-steam"><i>Steam</i></a></li>
+    <li><a href="#" class="fab fa-tumblr"><i>Tumblr</i></a></li>
+    <li><a href="#" class="fab fa-twitch"><i>Twitch</i></a></li>
+    <li><a href="#" class="fab fa-twitter"><i>Twitter</i></a></li>
+    <li><a href="#" class="fab fa-youtube"><i>Youtube</i></a></li>
 </ul>
