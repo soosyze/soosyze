935d89131756cbb61de0a667b920d956f41dc1d4
noelma <noel-mathieu@hotmail.fr>
2019-09-08 23:07:53 +0200

    Ajout d'une favicone par défaut.

--- core/themes/Admin/html.php
+++ core/themes/Admin/html.php
@@ -5,7 +5,6 @@
         <meta content="IE=edge" http-equiv="X-UA-Compatible">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title><?php echo $title ?></title>
-        <link rel="shortcut icon" href="<?php echo $base_theme; ?>assets/files/favicon.ico"/>
         <meta name="description" content="<?php echo $description; ?>"/>
         <meta name="keywords" content="<?php echo $keyboard; ?>"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
