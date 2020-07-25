68fe0577e69487089dc0c5938a4861ff4d533499
noelma <noel-mathieu@hotmail.fr>
2020-07-25 18:47:48 +0200

    Empêche la suppression des menus administration, principal et utilisateur.

--- core/modules/Menu/Services/HookUser.php
+++ core/modules/Menu/Services/HookUser.php
@@ -15,4 +15,11 @@ class HookUser
     {
         return 'menu.administer';
     }
+    
+    public function hookMenuDelete( $name )
+    {
+        return in_array($name, [ 'menu-main', 'menu-admin', 'menu-user' ])
+            ? false
+            : 'menu.administer';
+    }
 }
