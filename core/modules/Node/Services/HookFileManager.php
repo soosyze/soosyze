ebf8d1420a635f683d7f25586b4baa410b441584
noelma <noel-mathieu@hotmail.fr>
2020-08-06 11:57:25 +0200

    #174 Ajout du FileManager aux formulaires des contenus.

---
+++ core/modules/Node/Services/HookFileManager.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace SoosyzeCore\Node\Services;
+
+use Psr\Http\Message\ResponseInterface;
+use Soosyze\Components\Form\FormBuilder;
+
+class HookFileManager
+{
+    private $core;
+
+    private $hasFileManager;
+
+    /**
+     * @var \Soosyze\Components\Router\Router
+     */
+    private $router;
+
+    public function __construct($core, $module, $router)
+    {
+        $this->core   = $core;
+        $this->router = $router;
+
+        $this->hasFileManager = $module->has('FileManager');
+    }
+
+    public function hookNodeCreateForm(FormBuilder &$form, $content, $type)
+    {
+        if (!$this->hasFileManager) {
+            return;
+        }
+
+        $form->append('fields-fieldset', function ($form) {
+            $form->group('files-fieldset', 'div', function ($form) {
+                $response = '<div class="col-md-12">'
+                    . '<div class="alert alert-info">'
+                    . t('NOTE: You cannot add media files until you save the page. Just click \'Save\' on top')
+                    . '</div>'
+                    . '</div>';
+
+                $form->legend('files-legend', t('Files'))
+                    ->html('key', '<div:attr>:_content</div>', [
+                        '_content' => $response,
+                        'class'    => 'row',
+                        'id'       => 'filemanager'
+                ]);
+            });
+        });
+    }
+
+    public function hookNodeEditForm(FormBuilder &$form, $content)
+    {
+        if (!$this->hasFileManager) {
+            return;
+        }
+
+        $request = $this->router->getRequestByRoute('filemanager.show', [
+            ':path' => "/node/{$content[ 'type' ]}/{$content[ 'id' ]}"
+        ]);
+        $this->getFileManager($form, $request);
+    }
+
+    public function hookEntityForm(FormBuilder &$form, $content, $node, $entity)
+    {
+        if (!$this->hasFileManager) {
+            return;
+        }
+
+        $request = $this->router->getRequestByRoute('filemanager.show', [
+            ':path' => "/node/{$node[ 'type' ]}/{$node[ 'id' ]}/$entity"
+        ]);
+        $this->getFileManager($form, $request);
+    }
+
+    protected function getFileManager(FormBuilder &$form, $request)
+    {
+        $response = '<div class="col-md-12">'
+            . '<div class="alert alert-info">'
+            . t('You do not have the necessary permissions to use the file manager')
+            . '</div>'
+            . '</div>';
+
+        if ($this->core->callHook('app.granted.route', [ $request ])) {
+            $route    = $this->router->parse($request);
+            $response = $this->router->execute($route, $request);
+        }
+
+        $form->append('fields-fieldset', function ($form) use ($response) {
+            $form->group('files-fieldset', 'div', function ($form) use ($response) {
+                $form->legend('files-legend', t('Files'))
+                    ->html('key', '<div:attr>:_content</div>', [
+                        '_content' => $response,
+                        'class'    => 'row',
+                        'id'       => 'filemanager'
+                ]);
+            });
+        });
+    }
+}
--- core/modules/Node/Views/node-create.php
+++ core/modules/Node/Views/node-create.php
@@ -1,11 +1,25 @@
 
 <div class="row">
+    <?php if (!empty($node_fieldset_submenu)): ?>
     <div class="col-md-3 sticky">
         <?php echo $node_fieldset_submenu; ?>
     </div>
     <div class="col-md-9">
         <div class="tab-content">
             <?php echo $form; ?>
+        </div>
+    </div>
+    <?php else: ?>
+    <div class="col-md-12">
+        <div class="tab-content">
+            <?php echo $form; ?>
+        </div>
+    </div>
+    <?php endif; ?>
+
+    <div id="modal_filemanager" class="modal" role="dialog" aria-label="<?php echo t('File actions window'); ?>">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content"></div>
         </div>
     </div>
 </div>
