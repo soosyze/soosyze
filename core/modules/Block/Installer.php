2361881361ad7a0802ce08916794dbdc9f0e1d1f
noelma <noel-mathieu@hotmail.fr>
2020-09-05 23:42:41 +0200

    Ajout d'un bloc de signalement de bogue dans le footer du thème d'administration.

--- core/modules/Block/Installer.php
+++ core/modules/Block/Installer.php
@@ -59,9 +59,34 @@ class Installer extends \SoosyzeCore\System\Migration
     public function seeders(ContainerInterface $ci)
     {
         $ci->query()
-            ->insertInto('block', [ 'section', 'title', 'content', 'weight', 'pages' ])
-            ->values([ 'footer', '', '<p>Power by <a href="https://soosyze.com">SoosyzeCMS</a></p>',
-                1, '' ])
+            ->insertInto('block', [
+                'section', 'title',
+                'content',
+                'weight',
+                'visibility_pages', 'pages'
+            ])
+            ->values([
+                'content_footer', '',
+                '<div class="block-report_github">'
+                . '<p>'
+                . '<a href="https://github.com/soosyze/soosyze/issues" '
+                . 'rel="noopener noreferrer" '
+                . 'target="_blank" '
+                . 'title="' . t('Found an bug? Please report it on GitHub.') . '">'
+                . '<i class="fa fa-fw fa-bug" aria-hidden="true"></i> '
+                . t('Report a bug.')
+                . '</a>'
+                . '</p>'
+                . '</div>',
+                50,
+                true, 'admin/%'
+            ])
+            ->values([
+                'footer', '',
+                '<p>Power by <a href="https://soosyze.com">SoosyzeCMS</a></p>',
+                50,
+                false, ''
+            ])
             ->execute();
     }
 
