d6df4dacebac86d833b14cd02f3d4f9bd51fa0f6
noelma <noel-mathieu@hotmail.fr>
2020-09-23 22:34:23 +0200

    Correction des liens actifs de premier niveau du thème public.

--- core/themes/Fez/menu.php
+++ core/themes/Fez/menu.php
@@ -2,7 +2,7 @@
 <ul>
     <?php foreach ($menu as $link): ?>
 
-    <li class="<?php echo $link[ 'link_active' ]; ?> <?php if (!empty($link[ 'submenu' ])): echo 'parent'; endif; ?>">
+    <li class="depth-<?php echo $level; ?> <?php echo $link[ 'link_active' ]; ?> <?php if (!empty($link[ 'submenu' ])): echo 'parent'; endif; ?>">
         <a href="<?php echo $link[ 'link' ]; ?>"<?php if ($link[ 'target_link' ]): ?> target="_blank" rel="noopener noreferrer" <?php endif; ?>>
             <?php echo !empty($link['icon']) ? "<i class='{$link['icon']}' aria-hidden='true'></i> " : ''; ?><?php echo $link[ 'title_link' ]; ?>
 
