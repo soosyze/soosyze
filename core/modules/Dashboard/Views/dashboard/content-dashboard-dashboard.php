c8c4520bb58de1581d2f68f358dd25ba2a5b190a
noelma <noel-mathieu@hotmail.fr>
2020-10-24 11:38:08 +0200

    Mise à jour beta2.4

--- core/modules/Dashboard/Views/dashboard/content-dashboard-dashboard.php
+++ core/modules/Dashboard/Views/dashboard/content-dashboard-dashboard.php
@@ -53,7 +53,7 @@
                 <tbody>
                     <tr>
                         <th><?php echo t('Soosyze version'); ?></th>
-                        <td>1.0.0-beta2.3</td>
+                        <td>1.0.0-beta2.4</td>
                     </tr>
                     <tr>
                         <th><?php echo t('Environment'); ?></th>
