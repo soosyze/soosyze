599f2cfcc1344f5ba5106e8f81f92a93dd2c60f5
noelma <noel-mathieu@hotmail.fr>
2020-12-13 11:55:19 +0100

    Correction de l'enregistrement de l'icone et de la favicon dans les configurations.

--- core/modules/System/Services/HookConfig.php
+++ core/modules/System/Services/HookConfig.php
@@ -321,7 +321,7 @@ final class HookConfig implements \SoosyzeCore\Config\Services\ConfigInterface
 
     public function files(&$inputsFile)
     {
-        $inputFiles = [ 'logo', 'favicon' ];
+        $inputsFile = [ 'logo', 'favicon' ];
     }
 
     public function after(&$validator, $data, $id)
