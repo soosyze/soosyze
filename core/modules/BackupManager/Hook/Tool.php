b8aec5b428b3b3c86543da8302efb9fad4202e5f
noelma <noel-mathieu@hotmail.fr>
2021-05-23 16:42:19 +0200

    #236 update php7.2 ToolInterface

--- core/modules/BackupManager/Hook/Tool.php
+++ core/modules/BackupManager/Hook/Tool.php
@@ -1,20 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SoosyzeCore\BackupManager\Hook;
+
+use Soosyze\Components\Router\Router;
 
 class Tool implements \SoosyzeCore\System\ToolInterface
 {
     /**
-     * @var \Soosyze\Components\Router\Router
+     * @var Router
      */
     private $router;
 
-    public function __construct($router)
+    public function __construct(Router $router)
     {
         $this->router = $router;
     }
 
-    public function hookToolAdmin(array &$tools)
+    public function hookToolAdmin(array &$tools): void
     {
         $tools[ 'backupmanager' ] = [
             'description' => 'Backup of the CMS',
