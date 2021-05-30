3edd644fb63c86abb8a292d70000e2e2ab963f27
noelma <noel-mathieu@hotmail.fr>
2021-05-30 17:25:06 +0200

    #236 update php7.2 ExtendTheme

--- core/themes/Fez/Extend.php
+++ core/themes/Fez/Extend.php
@@ -4,12 +4,12 @@ namespace SoosyzeCore\Theme\Fez;
 
 class Extend extends \SoosyzeCore\System\ExtendTheme
 {
-    public function getDir()
+    public function getDir(): string
     {
         return __DIR__;
     }
 
-    public function boot()
+    public function boot(): void
     {
         $this->loadTranslation('fr', __DIR__ . '/Lang/fr/main.json');
     }
