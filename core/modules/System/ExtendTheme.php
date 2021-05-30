6078d9df7d85793bff7fdaae923e1f5ab793e761
noelma <noel-mathieu@hotmail.fr>
2021-05-30 17:25:06 +0200

    #236 update php7.2 ExtendTheme

--- core/modules/System/ExtendTheme.php
+++ core/modules/System/ExtendTheme.php
@@ -4,22 +4,25 @@ namespace SoosyzeCore\System;
 
 abstract class ExtendTheme
 {
+    /**
+     * @var array
+     */
     private $translations = [];
 
-    public function getTranslations()
+    public function getTranslations(): array
     {
         return $this->translations;
     }
 
-    public function loadTranslation($lang, $file)
+    public function loadTranslation(string $lang, string $file): void
     {
         $this->translations[ $lang ][] = $file;
     }
 
-    abstract public function getDir();
+    abstract public function getDir(): string;
 
     /**
-     * Chargement des Assets du module.
+     * Chargement des Assets du thème.
      */
-    abstract public function boot();
+    abstract public function boot(): void;
 }
