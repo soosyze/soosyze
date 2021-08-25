9e11633bc14ae5f0748d8813d03ac22e604aa9a7
noelma <noel-mathieu@hotmail.fr>
2021-08-25 22:11:38 +0200

    feat: standardize menu actions by HTTP methods

--- core/modules/Menu/Controller/Menu.php
+++ core/modules/Menu/Controller/Menu.php
@@ -10,7 +10,6 @@ use Soosyze\Components\Form\FormBuilder;
 use Soosyze\Components\Util\Util;
 use Soosyze\Components\Validator\Validator;
 use SoosyzeCore\Menu\Form\FormMenu;
-use SoosyzeCore\Template\Services\Block;
 
 class Menu extends \Soosyze\Controller
 {
@@ -19,104 +18,6 @@ class Menu extends \Soosyze\Controller
         $this->pathServices = dirname(__DIR__) . '/Config/services.php';
         $this->pathRoutes   = dirname(__DIR__) . '/Config/routes.php';
         $this->pathViews    = dirname(__DIR__) . '/Views/';
-    }
-
-    public function admin(ServerRequestInterface $req): ResponseInterface
-    {
-        return $this->show('menu-main', $req);
-    }
-
-    public function show(string $name, ServerRequestInterface $req): ResponseInterface
-    {
-        if (!($menu = self::menu()->getMenu($name)->fetch())) {
-            return $this->get404($req);
-        }
-
-        $action = self::router()->getRoute('menu.check', [ ':menu' => $name ]);
-
-        $form = (new FormBuilder([ 'action' => $action, 'class' => 'form-api', 'method' => 'patch' ]))
-            ->token('token_menu')
-            ->submit('submit', t('Save'), [ 'class' => 'btn btn-success' ]);
-
-        $messages = [];
-        if (isset($_SESSION[ 'messages' ])) {
-            $messages = $_SESSION[ 'messages' ];
-            unset($_SESSION[ 'messages' ]);
-        }
-
-        return self::template()
-                ->getTheme('theme_admin')
-                ->view('page', [
-                    'icon'       => '<i class="fa fa-bars" aria-hidden="true"></i>',
-                    'title_main' => t($menu[ 'title' ])
-                ])
-                ->view('page.messages', $messages)
-                ->view('page.submenu', $this->getMenuSubmenu('menu.show', $menu[ 'name' ]))
-                ->make('page.content', 'menu/content-menu-show.php', $this->pathViews, [
-                    'form'              => $form,
-                    'link_create_link'  => self::router()->getRoute('menu.link.create', [
-                        ':menu' => $name
-                    ]),
-                    'link_create_menu'  => self::router()->getRoute('menu.create'),
-                    'list_menu_submenu' => $this->getListMenuSubmenu($name),
-                    'menu'              => $this->renderMenu($name),
-                    'menu_name'         => $menu[ 'title' ]
-        ]);
-    }
-
-    public function check(string $name, ServerRequestInterface $req): ResponseInterface
-    {
-        $route = self::router()->getRoute('menu.show', [ ':menu' => $name ]);
-        if (!($links = self::menu()->getLinkPerMenu($name)->fetchAll())) {
-            return $this->json(200, [ 'redirect' => $route ]);
-        }
-
-        $validator = new Validator();
-        foreach ($links as $link) {
-            $validator
-                ->addRule("active-{$link[ 'id' ]}", 'bool')
-                ->addRule("parent-{$link[ 'id' ]}", 'required|numeric')
-                ->addRule("weight-{$link[ 'id' ]}", 'required|between_numeric:1,50');
-        }
-        $validator->addRule('token_menu', 'token')
-            ->setInputs($req->getParsedBody());
-
-        if ($validator->isValid()) {
-            $updateParents = [];
-            foreach ($links as $link) {
-                $data = [
-                    'active'       => $validator->getInput("active-{$link[ 'id' ]}") === 'on',
-                    'has_children' => false,
-                    'parent'       => (int) $validator->getInput("parent-{$link[ 'id' ]}"),
-                    'weight'       => (int) $validator->getInput("weight-{$link[ 'id' ]}")
-                ];
-
-                self::query()
-                    ->update('menu_link', $data)
-                    ->where('id', '=', $link[ 'id' ])
-                    ->execute();
-
-                if ($data[ 'parent' ] >= 1 && !in_array($data[ 'parent' ], $updateParents)) {
-                    $updateParents[] = $data[ 'parent' ];
-                }
-            }
-            /* Mise à jour des parents. */
-            foreach ($updateParents as $parent) {
-                self::query()
-                    ->update('menu_link', [ 'has_children' => true ])
-                    ->where('id', '=', $parent)
-                    ->execute();
-            }
-
-            $_SESSION[ 'messages' ][ 'success' ] = [ t('Saved configuration') ];
-
-            return $this->json(200, [ 'redirect' => $route ]);
-        }
-
-        return $this->json(400, [
-                'messages'    => [ 'errors' => $validator->getKeyErrors() ],
-                'errors_keys' => $validator->getKeyInputErrors()
-        ]);
     }
 
     public function create(ServerRequestInterface $req): ResponseInterface
@@ -210,7 +111,7 @@ class Menu extends \Soosyze\Controller
                     ])
                 ])
                 ->view('page.messages', $messages)
-                ->view('page.submenu', $this->getMenuSubmenu('menu.edit', $menu))
+                ->view('page.submenu', self::menu()->getMenuSubmenu('menu.edit', $menu))
                 ->make('page.content', 'menu/content-menu-form.php', $this->pathViews, [
                     'form' => $form
                 ]);
@@ -287,7 +188,7 @@ class Menu extends \Soosyze\Controller
                         ':name' => t($menu[ 'title' ])
                     ])
                 ])
-                ->view('page.submenu', $this->getMenuSubmenu('menu.remove', $name))
+                ->view('page.submenu', self::menu()->getMenuSubmenu('menu.remove', $name))
                 ->make('page.content', 'menu/content-menu-form.php', $this->pathViews, [
                     'form' => $form
                 ]);
@@ -328,6 +229,8 @@ class Menu extends \Soosyze\Controller
                 ->execute();
             $this->container->callHook('menu.delete.after', [ $validator, $menu ]);
 
+            $_SESSION[ 'messages' ][ 'success' ] = [ t('Saved configuration') ];
+
             return $this->json(200, [
                     'redirect' => self::router()->getRoute('menu.admin')
             ]);
@@ -337,104 +240,6 @@ class Menu extends \Soosyze\Controller
                 'messages'    => [ 'errors' => $validator->getKeyErrors() ],
                 'errors_keys' => $validator->getKeyInputErrors()
         ]);
-    }
-
-    public function renderMenu(string $nameMenu, int $parent = -1, int $level = 1): Block
-    {
-        $query = self::query()
-            ->from('menu_link')
-            ->where('menu', '=', $nameMenu)
-            ->where('parent', '=', $parent)
-            ->orderBy('weight')
-            ->fetchAll();
-
-        foreach ($query as &$link) {
-            $link[ 'link_edit' ]   = self::router()
-                ->getRoute('menu.link.edit', [ ':menu' => $link[ 'menu' ], ':id' => $link[ 'id' ] ]);
-            $link[ 'link_delete' ] = self::router()
-                ->getRoute('menu.link.delete', [ ':menu' => $link[ 'menu' ], ':id' => $link[ 'id' ] ]);
-            $link[ 'submenu' ]     = $link[ 'has_children' ]
-                ? $this->renderMenu($nameMenu, $link[ 'id' ], $level + 1)
-                : $this->createBlockMenuShowForm($nameMenu, null, $level + 1);
-
-            if (!$link[ 'key' ]) {
-                continue;
-            }
-
-            $link[ 'link' ] = self::menu()->rewiteUri($link['link'], $link['query'], $link['fragment']);
-        }
-        unset($link);
-
-        return $this->createBlockMenuShowForm($nameMenu, $query, $level);
-    }
-
-    public function getMenuSubmenu(string $keyRoute, string $nameMenu): array
-    {
-        $menu = [
-            [
-                'key'        => 'menu.show',
-                'request'    => self::router()->getRequestByRoute('menu.show', [
-                    ':menu' => $nameMenu
-                ]),
-                'title_link' => 'View'
-            ], [
-                'key'        => 'menu.edit',
-                'request'    => self::router()->getRequestByRoute('menu.edit', [
-                    ':menu' => $nameMenu
-                ]),
-                'title_link' => 'Edit'
-            ], [
-                'key'        => 'menu.remove',
-                'request'    => self::router()->getRequestByRoute('menu.remove', [
-                    ':menu' => $nameMenu
-                ]),
-                'title_link' => 'Delete'
-            ]
-        ];
-
-        $this->container->callHook('menu.submenu', [ &$menu ]);
-
-        foreach ($menu as $key => &$link) {
-            if (!self::core()->callHook('app.granted.request', [ $link[ 'request' ] ])) {
-                unset($menu[ $key ]);
-
-                continue;
-            }
-            $link[ 'link' ] = $link[ 'request' ]->getUri();
-        }
-
-        return [
-            'key_route' => $keyRoute,
-            'menu'      => $menu
-        ];
-    }
-
-    public function getListMenuSubmenu(string $nameMenu): Block
-    {
-        $menus = self::query()
-            ->from('menu')
-            ->fetchAll();
-
-        foreach ($menus as &$menu) {
-            $menu[ 'link' ] = self::router()
-                ->getRoute('menu.show', [ ':menu' => $menu[ 'name' ] ]);
-        }
-        unset($menu);
-
-        return self::template()
-                ->createBlock('menu/submenu-menu-list.php', $this->pathViews)
-                ->addVars([
-                    'key_route' => $nameMenu,
-                    'menu'      => $menus
-        ]);
-    }
-
-    private function createBlockMenuShowForm(string $nameMenu, ?array $query, int $level): Block
-    {
-        return self::template()
-                ->createBlock('menu/content-menu-show_form.php', $this->pathViews)
-                ->addNameOverride("menu-show-$nameMenu.php")
-                ->addVars([ 'level' => $level, 'menu' => $query ]);
     }
 
     private function getValidator(ServerRequestInterface $req): Validator
---
+++ core/modules/Menu/Views/menu/modal-form.php
@@ -0,0 +1,32 @@
+
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-label="<?php echo t('Close'); ?>">&times;</button>
+    <h2><?php echo $title; ?></h2>
+</div>
+
+<div class="modal-body <?php echo not_empty_or($class); ?>">
+    <div class="modal-messages"></div>
+
+    <?php echo not_empty_or($menu); ?>
+
+    <div class="block-edit">
+        <div class="row">
+            <?php if (!empty($fieldset_submenu)): ?>
+            <div class="col-md-3">
+                <?php echo $fieldset_submenu; ?>
+            </div>
+            <div class="col-md-9">
+                <div class="tab-content">
+                    <?php echo $form; ?>
+                </div>
+            </div>
+            <?php else: ?>
+            <div class="col-md-12">
+                <div class="tab-content">
+                    <?php echo $form; ?>
+                </div>
+            </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
