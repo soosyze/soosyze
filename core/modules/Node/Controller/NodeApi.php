64f22b98833667d280215e24d605bfc387906f87
noelma <noel-mathieu@hotmail.fr>
2021-08-29 13:43:44 +0200

    feat: api-first node module

--- core/modules/Node/Controller/NodeApi.php
+++ core/modules/Node/Controller/NodeApi.php
@@ -38,7 +38,7 @@ class NodeApi extends \Soosyze\Controller
 
         $this->container->callHook('node.remove.form.data', [ &$node, $idNode ]);
 
-        $action = self::router()->getRoute('node.api.delete', [ ':id_node' => $idNode ]);
+        $action = self::router()->getRoute('node.delete', [ ':id_node' => $idNode ]);
 
         $form = (new FormNodeDelete([ 'action' => $action, 'method' => 'delete' ], self::router()))
             ->setValues($values, $useInPath)
@@ -59,82 +59,5 @@ class NodeApi extends \Soosyze\Controller
                     'form'  => $form,
                     'title' => t('Delete :name content', [ ':name' => $node[ 'title' ] ])
         ]);
-    }
-
-    public function delete(int $idNode, ServerRequestInterface $req): ResponseInterface
-    {
-        if (!($node = self::node()->byId($idNode))) {
-            return $this->get404($req);
-        }
-
-        $validator = (new Validator())
-            ->setRules([
-                'id'    => 'required',
-                'files' => 'bool'
-            ])
-            ->setInputs([ 'id' => $idNode ] + $req->getParsedBody());
-
-        $pathsSettings = self::node()->getPathSettings();
-
-        foreach ($pathsSettings as $value) {
-            if (empty($value[ 'path' ]) && self::alias()->getSource($value[ 'path' ], $value[ 'path' ]) !== 'node/' . $idNode) {
-                continue;
-            }
-
-            $not = empty($value[ 'required' ])
-                ? ''
-                : '!';
-
-            $currentAlias = self::alias()->getalias('node/' . $idNode, 'node/' . $idNode);
-
-            $validator
-                ->addRule('path', $not . "required|route|!equal:$currentAlias|!equal:node/$idNode")
-                ->addInput('path_key', $value[ 'key' ])
-                ->addRule('path_key', $not . 'required|string')
-                ->addLabel('path', t('New path for') . ' ' . t($value[ 'title' ]))
-                ->setMessages([
-                    'path' => [
-                        'equal' => [
-                            'not' => t('You cannot enter the URL of the content that is going to be deleted.')
-                        ]
-                    ]
-                ]);
-
-            break;
-        }
-
-        $this->container->callHook('node.delete.validator', [ &$validator, $idNode ]);
-
-        $out = [];
-        if ($validator->isValid()) {
-            $this->container->callHook('node.delete.before', [ $validator, $idNode ]);
-            self::node()->deleteRelation($node);
-
-            self::query()
-                ->from('node')
-                ->delete()
-                ->where('id', '=', $idNode)
-                ->execute();
-
-            if ((bool) $validator->getInput('files')) {
-                self::node()->deleteFile($node[ 'type' ], $idNode);
-            }
-            $this->container->callHook('node.delete.after', [ $validator, $idNode ]);
-
-            if ($validator->getInput('path')) {
-                self::config()->set($validator->getInput('path_key'), $validator->getInput('path'));
-            }
-
-            $out[ 'messages' ][ 'success' ] = [
-                t('Content :title has been deleted', [ ':title' => $node[ 'title' ] ])
-            ];
-
-            return $this->json(200, $out);
-        }
-
-        $out[ 'messages' ][ 'errors' ] = $validator->getKeyErrors();
-        $out[ 'errors_keys' ]          = $validator->getKeyInputErrors();
-
-        return $this->json(400, $out);
     }
 }
