8bbbdd33ac291309bed8ff98e830c030d2283c3a
noelma <noel-mathieu@hotmail.fr>
2021-08-29 13:01:43 +0200

    feat: api-first block module

--- core/modules/Block/Config/routes.php
+++ core/modules/Block/Config/routes.php
@@ -17,17 +17,16 @@ define('BLOCK_WITH_ID', [
 R::useNamespace('SoosyzeCore\Block\Controller');
 
 R::get('block.section.admin', 'admin/theme/:theme/section', 'Section@admin', [ ':theme' => 'public|admin' ]);
-R::get('block.section.show', 'admin/section/:theme/:section', 'Section@show', BLOCK_WITH);
 R::post('block.section.update', 'admin/section/:id/edit', 'Section@update', [ ':id' => '\d+' ]);
 
 R::useNamespace('SoosyzeCore\Block\Controller')->name('block.')->prefix('block')->group(function () {
     R::get('create.list', '/:theme/create/:section', 'Block@createList', BLOCK_WITH);
     R::get('create.show', '/create/:id', 'Block@createShow', [ ':id' => '[\w\.\-]+' ]);
     R::post('create.form', '/:theme/create/form', 'Block@createForm', BLOCK_WITH_THEME);
-    R::post('store', '/:theme', 'Block@store', BLOCK_WITH_THEME);
 
+    R::post('store', '/:theme', 'Block@store', BLOCK_WITH_THEME);
     R::get('edit', '/:theme/:id/edit', 'Block@edit', BLOCK_WITH_ID);
     R::put('update', '/:theme/:id', 'Block@update', BLOCK_WITH_ID);
     R::get('remove', '/:theme/:id/delete', 'Block@remove', BLOCK_WITH_ID);
-    R::delete('delete', '/:theme/:id/delete', 'Block@delete', BLOCK_WITH_ID);
+    R::delete('delete', '/:theme/:id', 'Block@delete', BLOCK_WITH_ID);
 });
