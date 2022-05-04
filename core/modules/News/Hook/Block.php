56c7f3b942071fe267b5a16afe0e4980b5b974ff
noelma <noel-mathieu@hotmail.fr>
2022-05-04 18:52:54 +0200

    fix: string for select labels

--- core/modules/News/Hook/Block.php
+++ core/modules/News/Hook/Block.php
@@ -298,10 +298,10 @@ class Block implements \SoosyzeCore\Block\BlockInterface
             $form->legend('new-legend', t('Settings'))
                 ->group('limit-group', 'div', function ($form) use ($values) {
                     $options = [
-                        [ 'label' => 1, 'value' => 1 ],
-                        [ 'label' => 2, 'value' => 2 ],
-                        [ 'label' => 3, 'value' => 3 ],
-                        [ 'label' => 4, 'value' => 4 ]
+                        [ 'label' => '1', 'value' => 1 ],
+                        [ 'label' => '2', 'value' => 2 ],
+                        [ 'label' => '3', 'value' => 3 ],
+                        [ 'label' => '4', 'value' => 4 ]
                     ];
 
                     $form->label('limit-label', t('Number of items to show'))
