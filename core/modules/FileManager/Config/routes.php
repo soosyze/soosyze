614c340071b0436580b69ea06d921f063dee859a
noelma <noel-mathieu@hotmail.fr>
2022-06-06 19:57:03 +0200

    fix: file manager routes missing parameters

--- core/modules/FileManager/Config/routes.php
+++ core/modules/FileManager/Config/routes.php
@@ -13,8 +13,8 @@ define('FILEMANAGER_PATH_WITH', [ 'path' => '(/[-\w]+){0,255}' ]);
 RouteCollection::setNamespace('SoosyzeCore\FileManager\Controller')->prefix('/filemanager')->name('filemanager.')->group(function (RouteGroup $r): void {
     $r->prefix('/file')->name('file.')->setNamespace('\File')->group(function (RouteGroup $r): void {
         $r->get('show', '{path}{name}{ext}', '@show', FILEMANAGER_FILE_WITH);
-        $r->get('create', 'path', '@create', FILEMANAGER_PATH_WITH);
-        $r->post('store', 'path', '@store', FILEMANAGER_PATH_WITH);
+        $r->get('create', '{path}', '@create', FILEMANAGER_PATH_WITH);
+        $r->post('store', '{path}', '@store', FILEMANAGER_PATH_WITH);
         $r->get('edit', '{path}{name}{ext}/edit', '@edit', FILEMANAGER_FILE_WITH);
         $r->put('update', '{path}{name}{ext}', '@update', FILEMANAGER_FILE_WITH);
         $r->get('remove', '{path}{name}{ext}/delete', '@remove', FILEMANAGER_FILE_WITH);
@@ -25,7 +25,7 @@ RouteCollection::setNamespace('SoosyzeCore\FileManager\Controller')->prefix('/fi
     $r->prefix('/copy')->name('copy.')->setNamespace('\FileCopy')->group(function (RouteGroup $r): void {
         $r->get('admin', '{path}{name}{ext}', '@admin', FILEMANAGER_FILE_WITH);
         $r->post('update', '{path}{name}{ext}', '@update', FILEMANAGER_FILE_WITH);
-        $r->get('show', 'path', '@show', FILEMANAGER_PATH_WITH);
+        $r->get('show', '{path}', '@show', FILEMANAGER_PATH_WITH);
     });
     $r->prefix('/folder{path}')->withs(['path' => '(/[-\w]+){1,255}' ])->name('folder.')->setNamespace('\Folder')->group(function (RouteGroup $r): void {
         $r->get('create', '/create', '@create', FILEMANAGER_PATH_WITH);
