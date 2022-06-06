985f6ea260660a82069e9cff02a498a7434e6ce7
noelma <noel-mathieu@hotmail.fr>
2022-06-06 19:55:51 +0200

    fix: combined block updates in a single request

--- core/modules/Block/Controller/Section.php
+++ core/modules/Block/Controller/Section.php
@@ -6,7 +6,6 @@ namespace SoosyzeCore\Block\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Soosyze\Components\Http\Response;
 use Soosyze\Components\Validator\Validator;
 use SoosyzeCore\Template\Services\Block;
 
@@ -39,8 +38,8 @@ class Section extends \Soosyze\Controller
                 ])
                 ->make('page.content', 'block/content-section-admin.php', $this->pathViews, [
                     'content'          => $theme === 'admin'
-                        ? 'Edit public theme blocks'
-                        : 'Edit admin theme blocks',
+                    ? 'Edit public theme blocks'
+                    : 'Edit admin theme blocks',
                     'link_theme_index' => self::router()->generateUrl('system.theme.index'),
                     'link_section'     => self::router()->generateUrl('block.section.admin', [
                         'theme' => $theme === 'admin'
@@ -59,36 +58,70 @@ class Section extends \Soosyze\Controller
         }
 
         $validator = (new Validator())
-            ->setRules([
-                'weight'  => 'required|numeric|between_numeric:0,50',
-                'section' => 'required|string|max:50'
-            ])
+            ->setRules([ 'block' => 'array' ])
             ->setInputs((array) $req->getParsedBody());
+
+        if (!$validator->isValid()) {
+            return $this->json(400, [
+                'messages'    => [ 'errors' => $validator->getKeyErrors() ],
+                'errors_keys' => $validator->getKeyInputErrors()
+            ]);
+        }
 
         $this->container->callHook('block.section.update.validator', [
             &$validator, $id
         ]);
 
-        if ($validator->isValid()) {
-            $data = [
-                'weight'  => $validator->getInputInt('weight'),
-                'section' => $validator->getInputString('section')
-            ];
+        $blocks = $validator->getInputArray('block');
 
-            $this->container->callHook('block.section.update.before', [
-                $validator, &$data, $id
-            ]);
+        $validatorBlock = self::getValidator($blocks);
 
-            self::query()
-                ->update('block', $data)
-                ->where('block_id', '=', $id)
-                ->execute();
+        if ($validatorBlock->isValid()) {
+            foreach ($blocks as $idBlock => $value) {
+                $data = [
+                    'weight'  => $value[ 'weight' ],
+                    'section' => $value[ 'section' ]
+                ];
 
-            $this->container->callHook('block.section.update.after', [
-                $validator, $data, $id
+                $this->container->callHook('block.section.update.before', [
+                    $validator, &$data, $id
+                ]);
+
+                self::query()
+                    ->update('block', $data)
+                    ->where('block_id', '=', $idBlock)
+                    ->execute();
+
+                $this->container->callHook('block.section.update.after', [
+                    $validator, $data, $id
+                ]);
+            }
+
+            return $this->json(200, [
+                    'messages' => [ 'success' => [ t('Saved configuration') ] ]
             ]);
         }
 
-        return $this->json(200);
+        return $this->json(400, [
+                'messages'    => [ 'errors' => $validatorBlock->getKeyErrors() ],
+                'errors_keys' => $validatorBlock->getKeyInputErrors()
+        ]);
+    }
+
+    private static function getValidator(array $blocks = []): Validator
+    {
+        $validator = new Validator();
+
+        foreach ($blocks as $key => $value) {
+            $validator
+                ->addRule("block[$key][section]", 'required|string|max:50')
+                ->addRule("block[$key][weight]", 'required|numeric|between_numeric:0,50');
+            $validator
+                ->addInput("block[$key][section]", $value[ 'section' ] ?? null)
+                ->addInput("block[$key][weight]", $value[ 'weight' ] ?? null)
+            ;
+        }
+
+        return $validator;
     }
 }
