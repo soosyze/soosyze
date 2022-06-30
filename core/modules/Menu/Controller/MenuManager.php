1f94308cd8e4ebbcbb7c0e88250fdd5a228097c9
noelma <noel-mathieu@hotmail.fr>
2022-06-30 19:19:17 +0200

    fix: declare strict types

--- core/modules/Menu/Controller/MenuManager.php
+++ core/modules/Menu/Controller/MenuManager.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SoosyzeCore\Menu\Controller;
 
 use Psr\Http\Message\ResponseInterface;
--- core/modules/System/ExtendTheme.php
+++ core/modules/System/ExtendTheme.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SoosyzeCore\System;
 
 abstract class ExtendTheme
