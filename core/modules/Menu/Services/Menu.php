4e51a969db4d9d1665a186d255b9da6927a19e9f
noelma <noel-mathieu@hotmail.fr>
2022-06-30 18:08:03 +0200

    fix: active link

--- core/modules/Menu/Services/Menu.php
+++ core/modules/Menu/Services/Menu.php
@@ -368,7 +368,7 @@ class Menu
                 continue;
             }
 
-            $menu[ 'link_active' ] = $route === '/' . trim($menu[ 'link' ], '/')
+            $menu[ 'link_active' ] = strpos($route, '/' . trim($menu[ 'link' ], '/')) === 0
                 ? 'active'
                 : '';
 
