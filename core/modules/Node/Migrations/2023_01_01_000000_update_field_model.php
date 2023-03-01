0d817526cc52ab1fef4d30fa9ccc639401c4ed50
noelma <noel-mathieu@hotmail.fr>
2023-03-01 23:23:31 +0100

    fix: migration field_typ select and option

--- core/modules/Node/Migrations/2023_01_01_000000_update_field_model.php
+++ core/modules/Node/Migrations/2023_01_01_000000_update_field_model.php
@@ -1,5 +1,6 @@
 <?php
 
+use Soosyze\Core\Modules\Node\Model\Field\SelectOption;
 use Soosyze\Core\Modules\System\Contract\DatabaseMigrationInterface;
 use Soosyze\Queryflatfile\Request;
 use Soosyze\Queryflatfile\Schema;
@@ -10,21 +11,20 @@ return new class implements DatabaseMigrationInterface {
         $nodeTypes = $req
             ->from('node_type_field')
             ->leftJoin('field', 'field_id', '=', 'field.field_id')
-            ->where('field_type', '=', 'one_to_many')
+            ->where('field_type', '=', 'select')
             ->fetchAll();
 
         /** @phpstan-var array{ field_option: string, field_id: int, node_type: string } $type */
         foreach ($nodeTypes as $type) {
-            /** @phpstan-var array{ sort: int|string } $options */
+            /** @phpstan-var array<int, string> $options */
             $options = (array) json_decode($type['field_option'], true);
 
-            $options['sort'] = in_array($options['sort'], ['weight', 'asc'])
-                ? SORT_ASC
-                : SORT_DESC;
+            $selectOption = SelectOption::create();
+            foreach ($options as $key => $label) {
+                $selectOption->addOption($label, $key);
+            }
 
-            $req->update('node_type_field', [
-                'field_option' => json_encode($options)
-            ])
+            $req->update('node_type_field', ['field_option' => json_encode($selectOption)])
                 ->where('field_id', '=', $type['field_id'])
                 ->where('node_type', '=', $type['node_type'])
                 ->execute();
