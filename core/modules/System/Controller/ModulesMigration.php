ce97311c073ec8c3ebc50fe991a35c7ac69bd7c2
noelma <noel-mathieu@hotmail.fr>
2023-03-08 00:20:54 +0100

    fix: initialize query service on migration error

--- core/modules/System/Controller/ModulesMigration.php
+++ core/modules/System/Controller/ModulesMigration.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\ResponseInterface;
 use Soosyze\Components\Http\Redirect;
 
 /**
- * @method \Soosyze\Core\Modules\System\Services\Migration migration()
+ * @method \Soosyze\Core\Modules\System\Services\Migration   migration()
+ * @method \Soosyze\Core\Modules\QueryBuilder\Services\Query query()
  */
 class ModulesMigration extends \Soosyze\Controller
 {
@@ -29,7 +30,9 @@ class ModulesMigration extends \Soosyze\Controller
             self::migration()->migrate();
             $_SESSION[ 'messages' ][ 'success' ][] = t('The update is a success');
         } catch (\Exception $e) {
-            $_SESSION[ 'messages' ][ 'error' ][] = t('An error occurred during the update');
+            $_SESSION[ 'messages' ][ 'errors' ][] = t('An error occurred during the update');
+            /* Initialise le service de la base de données car celui-ci est un singleton */
+            self::query()->init();
         }
 
         return new Redirect(self::router()->generateUrl('system.module.edit'), 302);
